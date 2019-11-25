c86ea12ee34bdf6122b32ce37f7ca03c61dbf3ba
brettmadrid <brettmadrid9@gmail.com>
2019-11-24 23:13:22 -0800

    first pass at bubble sort

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,6 +1,15 @@
 // larger values in the array bubble to the top
-function BubbleSort(arr) {}
-
 const swap = (arr, idx1, idx2) => {
-  [arr[idx1], (arr[idx2] = arr[idx2]), arr[idx1]];
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
 };
+
+function BubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      swap(arr, i, i + 1);
+    }
+  }
+  return arr;
+}
+
+console.log(BubbleSort([5, 2, 3, 1, 4]));
