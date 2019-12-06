c14455d41a47d55102ed45941d860a72f3156d2f
brettmadrid <brettmadrid9@gmail.com>
2019-12-05 22:13:28 -0800

    completed quicksort recursive function

--- QuickSort.js
+++ QuickSort.js
@@ -16,3 +16,16 @@ function pivot(arr, start = 0, end = arr.length + 1) {
   swap(arr, start, swapIdx);
   return swapIdx;
 }
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    // left
+    quickSort(arr, left, pivotIndex - 1);
+    // right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([4, 8, 2, 1, 5, 76, 3]));
