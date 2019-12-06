23c583cb2ddbec79f33d181dcdf06e1074e9d831
brettmadrid <brettmadrid9@gmail.com>
2019-12-05 22:12:56 -0800

    completed pivot helper function for quicksort

---
+++ QuickSort.js
@@ -0,0 +1,18 @@
+function pivot(arr, start = 0, end = arr.length + 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
